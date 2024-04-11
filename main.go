a8ddf037c4f4f456710f7b3b95715377bc11c703
Mateusz Sobczyk <mateusz.sobczyk@acaisoft.com>
2024-04-11 06:04:14 +0200

    WIP

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
@@ -17,13 +20,31 @@ func main() {
 	}
 
 	fmt.Println("Sudoku before solving:")
-	printSudoku(sudokuArray)
+	rows, colls := printSudoku(sudokuArray)
+
+	maxRow := slices.Max(rows)
+	maxColl := slices.Max(colls)
+
+	maxRowIndex := slices.Index(rows, maxRow)
+	maxCollIndex := slices.Index(colls, maxColl)
+
+	if maxRow > maxColl {
+		fmt.Println(maxRowIndex)
+	} else {
+		fmt.Println(maxCollIndex)
+		findFirstZeroInLine(sudokuArray, maxCollIndex, false)
+	}
 }
 
-func printSudoku(sudokuArray [9][9]int) {
+func findFirstZeroInLine(sudokuArray [9][9]int, maxCollIndex int, false bool) {
+
+}
+
+func printSudoku(sudokuArray [9][9]int) ([]int, []int) {
+	var solvedRows []int
+	var solvedColls []int
 	for row := 0; row < 9; row++ {
 		printVerticalLine(row)
-		numOfSolved := 0
 		for col := 0; col < 9; col++ {
 			val := sudokuArray[row][col]
 			printHorizontalLine(col)
@@ -31,28 +52,27 @@ func printSudoku(sudokuArray [9][9]int) {
 			if val == 0 {
 				fmt.Print("  ")
 			} else {
-				fmt.Print(sudokuArray[row][col], " ")
-				numOfSolved++
+				fmt.Print(val, " ")
 			}
 		}
-		fmt.Print("    ", numOfSolved)
+		solvedInLine := countSolvedCellsInLine(sudokuArray, true, row)
+		solvedRows = append(solvedRows, solvedInLine)
+		fmt.Print("    ", solvedInLine)
 		fmt.Println()
 	}
 	fmt.Println()
 	fmt.Println()
-	for row := 0; row < 9; row++ {
-		numOfSolved := 0
-		for col := 0; col < 9; col++ {
-			val := sudokuArray[col][row]
-			if val != 0 {
-				numOfSolved++
-			}
-		}
-		printHorizontalLine(row)
-		fmt.Print(numOfSolved, " ")
+	for col := 0; col < 9; col++ {
+		solvedInLine := countSolvedCellsInLine(sudokuArray, false, col)
+		solvedColls = append(solvedColls, solvedInLine)
+		printHorizontalLine(col)
+		fmt.Print(solvedInLine, " ")
 	}
 	fmt.Println()
-
+	fmt.Println()
+	fmt.Println("SolvedRows: ", solvedRows)
+	fmt.Println("SolvedColls: ", solvedColls)
+	return solvedRows, solvedColls
 }
 
 func printVerticalLine(i int) {
@@ -65,4 +85,24 @@ func printHorizontalLine(j int) {
 	if j != 0 && j%3 == 0 {
 		fmt.Print("| ")
 	}
+}
+
+func countSolvedCellsInLine(sudokuArray [9][9]int, isRow bool, j int) int {
+	count := 0
+	for i := 0; i < 9; i++ {
+		val := 0
+		if isRow {
+			val = sudokuArray[j][i]
+		} else {
+			val = sudokuArray[i][j]
+		}
+		if val != 0 {
+			count++
+		}
+	}
+	return count
+}
+
+func findPossibilitiesForField(sudokuArray [9][9]int, row int, col int) []int {
+	panic("Not implemented")
 }
